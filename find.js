358321e63457a4cad2ef4f7182cce60106599c4e
rajat-saxena-16 <rajat.saxena.16@mountblue.tech>
2021-05-13 15:44:38 +0530

    find function added

---
+++ find.js
@@ -0,0 +1,20 @@
+function find(arr){
+if(arr == undefined || arr.length == 0 || Array.isArray(arr) === false){
+    return []
+    }
+else{
+    function base(arr){
+            for(var i=0;i<arr.length;i++){
+                if( cb(arr[i]) == true){
+                     return arr[i]       
+                }
+            }
+    }
+    function cb(ele){
+                return ele > 3
+            }
+        return base(arr,cb)
+    }
+}
+
+module.exports = find;
---
+++ test/testFind.js
@@ -0,0 +1,5 @@
+var find = require("../find")
+const array = [1,2,3,4]
+
+const result = find(array)
+console.log(result)
