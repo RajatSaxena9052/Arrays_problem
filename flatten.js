9f5941e4b7348e5dc3d622b191e91ce6879a96a8
rajat-saxena-16 <rajat.saxena.16@mountblue.tech>
2021-05-13 12:00:17 +0530

    last question

---
+++ flatten.js
@@ -0,0 +1,24 @@
+function fn(nestedArray){
+    if(typeof nestedArray != 'object' || nestedArray == undefined || nestedArray.length == 0){
+        return []
+    }
+    else{
+    var newArray = []
+    function flatten(nestedArray){
+
+        for(var i=0;i<nestedArray.length;i++){
+                if(typeof nestedArray[i] == 'object'){
+                    flatten(nestedArray[i])
+                }
+                else{
+                    newArray.push(nestedArray[i])
+                }
+            }
+            return newArray
+        }
+    
+        return flatten(nestedArray)
+    }
+}
+
+module.exports = fn;
---
+++ test/testFlatten.js
@@ -0,0 +1,6 @@
+var fn = require("../flatten")
+
+const nestedArray =[ 1, [2], [[3]], [[[4]]] ];
+
+const result = fn(nestedArray)
+console.log(result)
